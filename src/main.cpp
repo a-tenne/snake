4309e5820da79627fc4f52de8403241dd434660a
a-tenne <atenne@icloud.com>
2025-07-28 08:12:06 +0200

    added assets folder that gets copied to the binary directory

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,14 @@ constexpr const char *NAME = "Snake";
 constexpr int WINDOW_WIDTH = 700;
 constexpr int WINDOW_HEIGHT = 700;
 constexpr unsigned int SDL_FLAGS = SDL_INIT_VIDEO;
-#ifdef NDEBUG
-  constexpr const char *FONT_PATH = "Pixellettersfull-BnJ5.ttf";
-#else
-  constexpr const char *FONT_PATH = "../src/Pixellettersfull-BnJ5.ttf";
-#endif
+constexpr const char *FONT_PATH = "Pixellettersfull-BnJ5.ttf";
 constexpr int FRAME_RATE = 10;
 
 int
 main (int argc, char *argv[])
 {
-  Game game (NAME, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_FLAGS, FONT_PATH, FRAME_RATE);
+  Game game (NAME, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_FLAGS, FONT_PATH,
+             FRAME_RATE);
   game.run ();
   return 0;
 }
